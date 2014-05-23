0697499fee1c6a552491ee0433e9a9ec1316cc08
schowell <schowell@66327853-f82c-4931-98c6-363a207d639d>
2014-05-23 14:15:03 +0000

    Updated to save the output to a '*.patches' file and cleaned up some minor formatting.
    git-svn-id: file:///home/svn/sassie_dna/trunk@31 66327853-f82c-4931-98c6-363a207d639d

--- pdb2psfgen.py
+++ pdb2psfgen.py
@@ -1,4 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# Author:  Steven C. Howell
+# Purpose: Replace DNA sequence with another sequence
+# Created: 04/24/2014
+# $Id: pdb2psfgen.py,v 1.2 2014-05-23 14:15:03 schowell Exp $
+'''
+This script loads a pdb structure file of DNA, and creates a '*.patches' file
+with the psfgen patches needed to use psfgen to create the structure.
+After running this script, the patches can be pasted into a psfgen file.
+'''
+
 import sassie.sasmol.sasmol as sasmol
 import sys, string
 import time
@@ -7,7 +17,8 @@ allAtomPDB = sys.argv[1]
 m1 = sasmol.SasMol(0)
 m1.read_pdb(allAtomPDB)
 
-
+chain1 = 'J'
+chain2 = 'L'
     
 names = m1.resname()
 ids = m1.resid()
@@ -17,7 +28,7 @@ pdbStrings = ['.pdb','.PDB']
 psfgenFile = sys.argv[1]
 #psfgenFile = sys.argv[2]
 for ext, item in enumerate(pdbStrings):
-    psfgenFile = psfgenFile.replace(item, '.patchs')
+    psfgenFile = psfgenFile.replace(item, '.patches')
 
 outfile = open(psfgenFile, 'w')      # open the file
 timestr = time.strftime("created on %d %B %Y by 'pdb2psfgen.py'")
@@ -29,20 +40,19 @@ pyrStr = 'patch DEO1 '
 purStr = 'patch DEO2 '
 
 n = 0
-j = 0
-for j in xrange(ids.size):
-    i = ids[j]
+for (j, i) in enumerate(ids):
+    # only want this to happend once for each residue
     if n != i:
         n = i
         # print 'adding line %d' % i
         
-        if c[j] in ['A']:
+        if c[j] in chain1:
             dna = 'dna1:%d\n' %i
-        elif c[j] in ['B']:
+        elif c[j] in chain2:
             dna = 'dna2:%d\n' %i
         else:
-            print 'ERROR!!! residue from unknown dna chain'
-            dna = 'dna?:%d' % i
+            print 'ERROR!!! residue from unknown dna chain (protein ?)'
+            dna = 'protein:%d\n' % i
             
         if names[j] in pyr:
             outfile.write(pyrStr + dna)
@@ -54,3 +64,5 @@ for j in xrange(ids.size):
             print '\n\n ERROR!!! skipped unknown resname'
 
 outfile.close()            
+
+print 'COMPLETE'
