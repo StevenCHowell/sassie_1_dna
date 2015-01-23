9330f7953f4ec7441ca557f9595af54160cf80c0
schowell <schowell@66327853-f82c-4931-98c6-363a207d639d>
2015-01-23 02:12:07 +0000

    added entropy's python path
    git-svn-id: file:///home/svn/sassie_dna/trunk@116 66327853-f82c-4931-98c6-363a207d639d

--- drivers/parallel_crysol.py
+++ drivers/parallel_crysol.py
@@ -169,7 +169,7 @@ def collect_crysol(sub_dirs, runname, sleep):
                 logging.debug('moving %s' % sub_file)
                 file_name = sub_file[:-4]
                 new_name = runname + '_' + str(n_out_files + j).zfill(5)
-                logging.debug('moving %s/%s.int to %s/%s.int' % (sub_dir, 
+                logging.debug('moving %s%s.int to %s/%s.int' % (sub_dir, 
                                     file_name, out_dir, new_name))
                 os.system('mv %s.int %s/%s.int' % (file_name, out_dir, new_name))
                 os.system('mv %s.log %s/%s.log' % (file_name, out_dir, new_name))                
@@ -183,6 +183,9 @@ def iterate_crysol(sub_dirs, dcd_file_names):
     if os.path.exists('/share/apps/bin/python'):
         #gibbs
         python = '/share/apps/bin/python'
+    elif os.path.exists('/share/apps/local/bin/python/bin/python'):
+        #entropy
+        python = '/share/apps/local/bin/python/bin/python'
     elif os.path.exists('/usr/bin/python'):
         #my machines
         python = '/usr/bin/python'
