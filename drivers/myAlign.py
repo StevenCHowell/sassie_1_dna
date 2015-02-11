331d9d6bced06827630926a43d8c28e0017434f5
schowell <schowell@66327853-f82c-4931-98c6-363a207d639d>
2015-02-11 03:35:33 +0000

    not sure
    git-svn-id: file:///home/svn/sassie_dna/trunk@120 66327853-f82c-4931-98c6-363a207d639d

--- drivers/myAlign.py
+++ drivers/myAlign.py
@@ -27,7 +27,7 @@ def parse():
         #epilog = 'no epilog found'
     )
 
-    parser.add_argument("-d", "--dest",    help="destination pdb")
+    parser.add_argument("-g", "--goal",    help="goal pdb")
     parser.add_argument("-r", "--ref",     help="reference pdb")
     parser.add_argument("-m", "--move",    help="pdb/dcd to align")
     parser.add_argument("-o", "--out",     help="output dcd file")
@@ -39,7 +39,7 @@ def parse():
     return parser.parse_args()
 
 def main():
-    aa_dest_pdb    = ARGS.dest
+    aa_goal_pdb    = ARGS.goal
     aa_move_pdb    = ARGS.ref
     aa_move_file   = ARGS.move
     save_file      = ARGS.out
@@ -51,12 +51,12 @@ def main():
 
     # create the SasMol objects
     aa_move  = sasmol.SasMol(0)
-    sub_dest = sasmol.SasMol(0)
+    sub_goal = sasmol.SasMol(0)
     sub_move = sasmol.SasMol(0)
-    aa_dest  = sasmol.SasMol(0)
+    aa_goal  = sasmol.SasMol(0)
     aa_move  = sasmol.SasMol(0)
 
-    aa_dest.read_pdb(aa_dest_pdb)
+    aa_goal.read_pdb(aa_goal_pdb)
     aa_move.read_pdb(aa_move_pdb)
 
     if aa_move_file[-3:] == 'pdb':
@@ -64,7 +64,7 @@ def main():
         n_frames = aa_move.number_of_frames()
         in_type = 'pdb'
     elif aa_move_file[-3:] == 'dcd':
-        dcd_file = aa_move.open_dcd_read(path+infile)
+        dcd_file = aa_move.open_dcd_read(aa_move_file)
         n_frames = dcd_file[2]
         in_type = 'dcd'
     else:
@@ -72,48 +72,48 @@ def main():
         print_failure(message, txtOutput) 
         return
 
-    if 'dcd' == save_file[-3:]:
+    dcd_out_file = None
+    out_type = save_file[-3:]
+    if 'dcd' == out_type:
         dcd_out_file = aa_move.open_dcd_write(path+save_file)
 
     basis_filter = "((segname[i] == '%s') and (name[i] == 'CA') and (resid[i] >= %s) and (resid[i] <= %s))" % (match_segname, match_res_min, match_res_max)
 
-    error, dest_seg_mask = aa_dest.get_subset_mask(basis_filter)
+    error, goal_seg_mask = aa_goal.get_subset_mask(basis_filter)
     error, move_seg_mask = aa_move.get_subset_mask(basis_filter)
 
-    error = aa_dest.copy_molecule_using_mask(sub_dest, dest_seg_mask, 0)
+    error = aa_goal.copy_molecule_using_mask(sub_goal, goal_seg_mask, 0)
     error = aa_move.copy_molecule_using_mask(sub_move, move_seg_mask, 0)
 
-    com_sub_dest = sub_dest.calccom(0)                            # calculate the center of mass of the subset of m1
-    sub_dest.center(0)                                           # center the m1 coordinates
-    coor_sub_dest = sub_dest.coor()[0]                            # get the m1 centered coordinates
+    com_sub_goal = sub_goal.calccom(0)                            # calculate the center of mass of the subset of m1
+    sub_goal.center(0)                                           # center the m1 coordinates
+    coor_sub_goal = sub_goal.coor()[0]                            # get the m1 centered coordinates
 
     for i in xrange(n_frames):
         if in_type == 'dcd':
             aa_move.read_dcd_step(dcd_file, i)
-            aa_move.center(i)                                            # move m2 to be centered at the origin
+            aa_move.center(0)                                            # move m2 to be centered at the origin
             error, sub_move.coor = aa_move.get_coor_using_mask(0, move_seg_mask)
             sub_move.setCoor(sub_move.coor)
             com_sub_move = sub_move.calccom(0)                            # calculate the center of mass of the subset of m2
             sub_move.center(0)                                           # move the subset of m2 to be centered at the origin
-            coor_sub_move = sub_move.coor()[0]                              # get the new coordinates of the subset of m2
-            aa_move.align(i,coor_sub_move,com_sub_move,coor_sub_dest,com_sub_dest) # align m2 using the transformation from sub_m2 to sub_m1
+            coor_sub_move = sub_move.coor[0]                              # get the new coordinates of the subset of m2
+            aa_move.align(i,coor_sub_move,com_sub_move,coor_sub_goal,com_sub_goal) # align m2 using the transformation from sub_m2 to sub_m1
         elif in_type == 'pdb':
             aa_move.center(i)                                            # move m2 to be centered at the origin
             error, sub_move.coor = aa_move.get_coor_using_mask(i, move_seg_mask)
             sub_move.setCoor(sub_move.coor)
             com_sub_move = sub_move.calccom(0)                            # calculate the center of mass of the subset of m2
             sub_move.center(0)                                           # move the subset of m2 to be centered at the origin
-            coor_sub_move = sub_move.coor()[0]                              # get the new coordinates of the subset of m2
-            aa_move.align(i,coor_sub_move,com_sub_move,coor_sub_dest,com_sub_dest) # align m2 using the transformation from sub_m2 to sub_m1
+            coor_sub_move = sub_move.coor[0]                              # get the new coordinates of the subset of m2
+            aa_move.align(i,coor_sub_move,com_sub_move,coor_sub_goal,com_sub_goal) # align m2 using the transformation from sub_m2 to sub_m1
 
-        a2.write_frame_to_file(aa_move,n_frames, i+1, path, save_file, dcd_out_file, in_type)
+        a2.write_frame_to_file(aa_move,n_frames, i+1, path, save_file, out_type, dcd_out_file, in_type)
 
     if(intype == 'dcd'):
-        m2.close_dcd_read(dcdfile[0])
+        aa_move.close_dcd_read(dcd_file[0])
     if(outtype == 'dcd'):
-        m2.close_dcd_write(dcdoutfile)
-    # save the output
-    aa_move.write_pdb("after.pdb",0,'w')     
+        aa_move.close_dcd_write(dcd_out_file)
     print 'COMPLETE \m/ >.< \m/'
 
 if __name__ == "__main__":
